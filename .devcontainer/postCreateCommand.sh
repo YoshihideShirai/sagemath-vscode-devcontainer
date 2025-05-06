4b7159e97fe3955a1aed38576ba19a6235d103e4
Yoshihide Shirai <yoshihide.shirai@gmail.com>
2025-05-06 06:00:43 +0000

    update

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -3,3 +3,4 @@
 sage -python3 -m pip install --upgrade pip
 sage -python3 -m pip install ipykernel
 sudo sage -python3 -m ipykernel install --name "sagemath" --display-name "SageMath"
+sudo cp -r ~/sage/local/var/lib/sage/venv-python3.12.5/share/jupyter/kernels/sagemath /usr/local/share/jupyter/kernels/
